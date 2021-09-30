8dcb3d5a54e0fd4ea87a6c2f22341d4423cbaa94
Kid <kidonng@gmail.com>
2021-09-30 20:02:50 +0800

    Add ZipFS VSCode extension

--- .config/fish/functions/exa.fish
+++ .config/fish/functions/exa.fish
@@ -3,11 +3,12 @@ command -sq exa || exit
 function exa
     set -p argv --group-directories-first --git
 
-    # https://github.com/ogham/exa/issues/879#issuecomment-845233451
     if isatty stdout
         command exa $argv
     else
+        # https://github.com/ogham/exa/issues/955
         set -p argv --color never
+        # https://github.com/ogham/exa/issues/879#issuecomment-845233451
         COLUMNS=1 command exa $argv
     end
 end
