e33cb883964d9ec7cae5bcbf69518c7c282211c4
Kid <hi@xuann.wang>
2023-04-24 23:01:45 +0800

    Forgot to update variable name

--- .config/fish/functions/file_to_data_uri.fish
+++ .config/fish/functions/file_to_data_uri.fish
@@ -1,6 +1,6 @@
 command --query base64 || exit
 
-function file_to_date_uri --argument-names img
+function file_to_date_uri --argument-names file
     # Do not use `openssl base64` which wraps output
-    echo "data:"(file --brief --mime-type $img)";base64,"(base64 -i $img)
+    echo "data:"(file --brief --mime-type $file)";base64,"(base64 -i $file)
 end
