48329d562391278382bf7fed44f1e76cf1614464
Kid <hi@xuann.wang>
2023-04-24 23:02:32 +0800

    Update nerd fonts downloader

--- .config/fish/scripts/download-jetbrains-mono-nerd-fonts.fish
+++ .config/fish/scripts/download-jetbrains-mono-nerd-fonts.fish
@@ -1,5 +1,5 @@
 for weight in Bold BoldItalic ExtraBold ExtraBoldItalic ExtraLight ExtraLightItalic Italic Light LightItalic Medium MediumItalic Regular SemiBold SemiBoldItalic Thin ThinItalic
-    set --local name "JetBrains Mono "(string replace --regex "(.)Italic" '$1 Italic' $weight)" Nerd Font Complete.ttf"
+    set --local name "JetBrains Mono Nerd Font Complete "(string replace --regex "(.)Italic" '$1 Italic' $weight)".ttf"
 
     curl -R -o $name "https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/JetBrainsMono/Ligatures/$weight/complete/"(string replace --all " " %20 $name)
 end
