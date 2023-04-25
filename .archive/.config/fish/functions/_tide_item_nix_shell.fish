8716e6769e9c9bc9ea8d84ec40b1576232610674
Kid <hi@xuann.wang>
2023-04-26 01:01:41 +0800

    Remove Tide nix shell & private item
    They are now built-in

--- .archive/.config/fish/functions/_tide_item_nix_shell.fish
+++
@@ -1,7 +0,0 @@
-function _tide_item_nix_shell
-    set -q tide_nix_color || set -lx tide_nix_color blue
-    set -q tide_nix_bg_color || set -lx tide_nix_bg_color normal
-    set -q tide_nix_icon || set -l tide_nix_icon 
-
-    set -q IN_NIX_SHELL && _tide_print_item nix $tide_nix_icon' ' $IN_NIX_SHELL
-end
--- .archive/.config/fish/functions/_tide_item_private.fish
+++
@@ -1,7 +0,0 @@
-function _tide_item_private
-    set -q tide_private_color || set -lx tide_private_color white
-    set -q tide_private_bg_color || set -lx tide_private_bg_color normal
-    set -q tide_private_icon || set -l tide_private_icon 
-
-    test -n "$fish_private_mode" && _tide_print_item private $tide_private_icon' '
-end
