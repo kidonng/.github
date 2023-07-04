d789882aa96c78a069f3d6258e8aa026946aa088
Kid <hi@xuann.wang>
2023-07-04 22:33:25 +0800

    Fix completion command name

--- .config/fish/completions/spot.fish
+++
@@ -1,2 +0,0 @@
-complete spot -s a -l add -d "Add exclusions"
-complete spot -s r -l remove -d "Remove exclusions"
---
+++ .config/fish/completions/spotlight_exclusions.fish
@@ -0,0 +1,2 @@
+complete spotlight_exclusions -s a -l add -d "Add exclusions"
+complete spotlight_exclusions -s r -l remove -d "Remove exclusions"
