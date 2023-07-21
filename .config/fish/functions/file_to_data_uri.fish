c8910dec568b719626ef3fc7d8db32f6951de508
Kid <hi@xuann.wang>
2023-07-21 15:00:53 +0800

    Fix typo

--- .config/fish/functions/file_to_data_uri.fish
+++ .config/fish/functions/file_to_data_uri.fish
@@ -1,6 +1,6 @@
 command --query base64 || exit
 
-function file_to_date_uri --argument-names file
+function file_to_data_uri --argument-names file
     # Do not use `openssl base64` which wraps output
     echo "data:"(file --brief --mime-type $file)";base64,"(base64 -i $file)
 end
